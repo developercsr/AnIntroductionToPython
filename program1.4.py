00b4a228c702259a5d19307699fe51d6d60ba8f3
sumanth <sumanthroyal111@gmail.com>
2024-01-03 23:47:30 +0530

    Logical Operators

---
+++ program1.4.py
@@ -0,0 +1,8 @@
+"""There are 3 logical Operators
+and : or : not
+1 ==> True
+0 ==> False """
+
+print((1 and 1),(0 and 1),(1 and 0),(0 and 0))
+print((1 or 1),(0 or 1),(1 or 0),(0 or 0))
+print((not 1), ( not 0))
