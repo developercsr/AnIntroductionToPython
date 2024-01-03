5235f4ad1fdd59aeb38aafdca8105867e36cc5bc
sumanth <sumanthroyal111@gmail.com>
2024-01-03 23:38:16 +0530

    Relational Operators

---
+++ python1.3.py
@@ -0,0 +1,9 @@
+"""There are 6 types of Relational Operators
+They are == : != : > : < : <= : >=
+return a boolean value"""
+print(5==5,5==4)
+print(5!=5,5!=4)
+print(2>3,3>2)
+print(2<3,3<2)
+print(5>=5 , 5>=4 ,5>=6)
+print(5<=5,5<=4,5<=6)
