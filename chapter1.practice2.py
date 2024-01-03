a0948fb60ec1d44726adcc3f751e8e87e45e3471
sumanth <sumanthroyal111@gmail.com>
2024-01-04 02:42:31 +0530

    math module functions are inxluded

--- chapter1.practice2.py
+++ chapter1.practice2.py
@@ -3,6 +3,8 @@ x=int(input("Enter the value of x : "))
 y=int(input("Enter the value of Y : "))
 res=2*math.cos((x+y)/2)*math.cos((x-y)/2)+math.exp(x)-1-(x/4)+math.tan(x)-math.log(y)
 print(res)
+print(math.ceil(3.5))
+print(math.floor(3.5))
 """
 Mtah Module is Used For
 math.cos()
@@ -13,4 +15,6 @@ math.pow(x,y)
 math.radinas()
 math.pi
 math.degrees(radians_value)
+math.floor()
+math.ceil()
 """
