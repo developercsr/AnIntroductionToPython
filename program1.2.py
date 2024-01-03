9932d377cba5766c89d104cc65dda1a22b3eb1d8
sumanth <sumanthroyal111@gmail.com>
2024-01-03 23:31:14 +0530

    Strings Printing

---
+++ program1.2.py
@@ -0,0 +1,10 @@
+def main():
+    print("hari om") # Can Use double codes
+    print("om namah shivai") # can use single codes
+    print("Bharath\tHas\t3\tColours")#\t is used to give space
+    print("Subhash Chandra Bose \n is my \n favorite person")#prints in 3 lines after\n
+    print("""namste1
+          namaste2
+          namste3""") # 3^ple quotes are used for Multi Liner string
+if __name__ == "__main__":
+    main()
