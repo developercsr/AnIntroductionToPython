882d795c8f1750019d288ce7e723fc9d6ec4fae7
sumanth <sumanthroyal111@gmail.com>
2024-01-04 00:15:33 +0530

    Variables,Assignment

---
+++ program1.6.py
@@ -0,0 +1,15 @@
+"""
+About Variables and Assignment
+In Python Variables should be assiugned  After They are declared"""
+x=5
+y=7
+z=7+8j
+"""Python is dynamic programing Language,So we need not to specify Any datatype
+It will be automatically takes that values"""
+print(x ,y ,z)
+#If we decalre Multiple varibles at for 1 variable it will take the last one 
+x=1
+x=4.5
+print(x)
+#If we want to know type of the variable we can use type() function
+print(type(x),type(y),type(z))
