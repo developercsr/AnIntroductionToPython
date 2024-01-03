13c96249624adaac67caca05f6e7682230d43dc8
sumanth <sumanthroyal111@gmail.com>
2024-01-03 23:19:36 +0530

    Errors are also added

--- program1.1.py
+++ program1.1.py
@@ -3,9 +3,12 @@ def main():
     print(x)
     x=7687686.87764876876#it is dynamic
     print(x)
+    print(5/2)#Gives Integer Divison result means float
+    print(6//3)#Gives quotient
+    print(5%2)#Gives Reminder
     #Order of arthematic Operations () > ** > -(negetion) > / >//> * > % > + > -
     print(10+(3*5)-10**2/5+10//5-10%2)
-    print(7/0)#ZeroDivisonError
-    print(77+6(6*7))#TypeError
+    print(7/0)        #====>ZeroDivisonError
+    print(77+6(6*7))   #====>TypeError
 if __name__ == "__main__":
     main()
